f291554fd21eded37a87606d0ee29e2fcd8ca15c
Joey Imlay <jo.imlay@gmail.com>
2022-05-10 12:26:41 +0100

    create and test sonarSweepPlus for pt2

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-const { sonarSweep } = require('../index');
+const { sonarSweep, sonarSweepPlus } = require('../index');
 
 const testData = [
   199, 200, 208, 210, 200, 207, 240, 269, 260, 263
@@ -7,5 +7,11 @@ const testData = [
 describe('#sonarSweep', () => {
   test('counts the number of times a depth measurement increases', () => {
     expect(sonarSweep(testData)).toBe(7);
+  });
+});
+
+describe('#sonarSweepPlus', () => {
+  test('counts the number of times a measurement window increases', () => {
+    expect(sonarSweepPlus(testData)).toBe(5);
   });
 });
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const sonarSweep = data => data.filter((n, i) =>  n > data[i - 1]).length;
+const sonarSweep = data => data.filter((n, i) => n > data[i - 1]).length;
 
-module.exports = { sonarSweep };
+const sonarSweepPlus = (data) => {
+  const windows = data.reduce((acc, n, i) => {
+    acc.push(data.slice(i, i+3).reduce((a,b) => a + b));
+    return acc;
+  }, []);
+
+  return sonarSweep(windows);
+}
+
+module.exports = { sonarSweep, sonarSweepPlus };
